2ba7914212a6d9365619ad8e0e24b4198de49e55
Bui Quoc Huy <wuochuy02@gmail.com>
2024-04-18 23:04:35 +0700

    Show the comments of a post

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -7,20 +7,24 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
     },
+
     password: {
       type: String,
       required: true,
     },
+
     profilePicture: {
       type: String,
       default:
         "https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw1UkTSZCFG_22sEiizuSezJ&ust=1712291158870000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMie8fnbp4UDFQAAAAAdAAAAABAE",
     },
+
     isAdmin: {
       type: Boolean,
       default: false,
