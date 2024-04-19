8cb46f268e0fdb90a7c9c8982ae144df6338d504
Bui Quoc Huy <wuochuy02@gmail.com>
2024-04-19 15:08:47 +0700

    Add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import {
   HiDocumentText,
   HiOutlineUserGroup,
   HiAnnotation,
+  HiChartPie,
 } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
@@ -47,6 +48,18 @@ export default function DashSidebar() {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
+          {currentUser && currentUser.isAdmin && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item
+                active={tab === "dash" || !tab}
+                icon={HiChartPie}
+                as="div"
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
@@ -58,6 +71,7 @@ export default function DashSidebar() {
               Profile
             </Sidebar.Item>
           </Link>
+
           {currentUser.isAdmin && (
             <Link to="/dashboard?tab=posts">
               <Sidebar.Item
@@ -69,6 +83,7 @@ export default function DashSidebar() {
               </Sidebar.Item>
             </Link>
           )}
+
           {currentUser.isAdmin && (
             <>
               <Link to="/dashboard?tab=users">
@@ -80,6 +95,7 @@ export default function DashSidebar() {
                   Users
                 </Sidebar.Item>
               </Link>
+
               <Link to="/dashboard?tab=comments">
                 <Sidebar.Item
                   active={tab === "comments"}
@@ -91,6 +107,7 @@ export default function DashSidebar() {
               </Link>
             </>
           )}
+
           <Sidebar.Item
             icon={HiArrowSmRight}
             className="cursor-pointer"
